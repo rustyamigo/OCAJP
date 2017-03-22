0402dd780a0e3cb5900a8a55f2b805d574c6a72f
Khozema Nullwala <Khozema.Nullwala@in.niit.com>
2017-03-22 17:43:49 +0530

    Commit on 22nd March, 2017

---
+++ arrays/ArrayEx01.java
@@ -0,0 +1,21 @@
+public class ArrayEx01 {
+
+	public static void main(String ... args) {
+	
+	
+		int intArray [] = new int[] {1,2,3,4,5};
+		String objArray [] = new String [] {"Ranbir", "Khozema", "Aakash"};
+
+		// printing the lenght of the array
+		System.out.println(intArray.length);
+		System.out.println(objArray.length);
+
+
+		// print the value of the array
+		System.out.println(intArray[0]);
+		System.out.println(objArray[0]);
+	
+	
+	}	
+
+}
