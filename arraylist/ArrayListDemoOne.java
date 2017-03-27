2608874405dc64ac7608e1a7e88643508f823e80
Khozema Nullwala <khozema.nullwala@gmail.com>
2017-03-27 07:29:54 +0530

    27th March 2017

---
+++ arraylist/ArrayListDemoOne.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+class ArrayListDemoOne{
+	public static void main(String[] args) {
+		
+		List list = new ArrayList();
+		int a = 1;
+		list.add(a);
+		list.add(1);
+
+		System.out.println(list);
+	}
+}
+
